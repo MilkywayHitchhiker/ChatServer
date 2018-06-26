1113b164bae9a15e5622e6f503c9d2b0ca409b26
jung wook OH <dark112365@naver.com>
2018-06-27 02:42:51 +0900

    LFQ 수정

--- ChatServer/ChatServer/LockFreeQueue.h
+++ ChatServer/ChatServer/LockFreeQueue.h
@@ -33,13 +33,13 @@ private :
 	_TOP_NODE *_pHead;
 	_TOP_NODE *_pTail;
 
-	CMemoryPool<NODE> *_pMemPool;
+
 
 	volatile __int64 _UniqueueNum;
 	volatile __int64 _NodeCnt;
 	volatile __int64 _MaxCnt;
 public :
-
+	CMemoryPool<NODE> *_pMemPool;
 	/*//////////////////////////////////////////////////////////////////////
 	//생성자.파괴자.
 	//////////////////////////////////////////////////////////////////////*/
@@ -47,7 +47,9 @@ public :
 	{
 		_TOP_NODE *HNode = ( _TOP_NODE * )_aligned_malloc (sizeof (_TOP_NODE), 16);
 		_TOP_NODE *TNode = ( _TOP_NODE * )_aligned_malloc (sizeof (_TOP_NODE), 16);
-		HNode->pNode = ( NODE * )malloc (sizeof (NODE));
+		_pMemPool = new CMemoryPool<NODE> (0);
+
+		HNode->pNode = _pMemPool->Alloc ();
 		HNode->pNode->pNext = NULL;
 		HNode->UNIQUEUE = 0;
 		
@@ -60,7 +62,7 @@ public :
 		_pHead = HNode;
 		_pTail = TNode;
 
-		_pMemPool = new CMemoryPool<NODE> (0);
+
 	}
 	~CQueue_LF ()
 	{
@@ -73,7 +75,7 @@ public :
 				return;
 			}
 		}
-		delete _pMemPool;
+		_pMemPool->Free (_pHead->pNode);
 	}
 
 
@@ -94,12 +96,12 @@ public :
 
 		_TOP_NODE PreNode;
 
-
 		NODE *pNode = _pMemPool->Alloc ();
 		pNode->Data = Data;
-		pNode->pNext = NULL;
+
 
 		INT64 Uniqueue = InterlockedIncrement64 (&_UniqueueNum);
+
 
 		while ( 1 )
 		{
@@ -107,40 +109,21 @@ public :
 			PreNode.UNIQUEUE = _pTail->UNIQUEUE;
 
 			//Tail의 next가 NULL이 아닐 경우 이미 누군가가 먼저 노드를 넣었으므로 Tail을 밀어줌.
+
 			if ( PreNode.pNode->pNext != NULL )
 			{
-				InterlockedCompareExchangePointer (( volatile PVOID * )&_pTail->pNode, _pTail->pNode->pNext, PreNode.pNode);
-
-				if ( PreNode.pNode == PreNode.pNode->pNext )
-				{
-					NODE *pNodeBuf = _pHead->pNode;
-					while ( 1 )
-					{
-						if ( pNodeBuf == NULL )
-						{
-							NODE *ptNodeBuf = _pTail->pNode;
-							while ( 1 )
-							{
-								if ( ptNodeBuf == NULL )
-								{
-									return false;
-								}
-								LOG_LOG (L"LF_Queue", LOG_SYSTEM, L"Node Recursive _pTail Node = %p pNext = %p", ptNodeBuf, ptNodeBuf->pNext);
-								ptNodeBuf = ptNodeBuf->pNext;
-							}
-						}
-						LOG_LOG (L"LF_Queue", LOG_SYSTEM, L"Node Recursive _pHead Node = %p pNext = %p", pNodeBuf, pNodeBuf->pNext);
-						pNodeBuf = pNodeBuf->pNext;
-					}
-				}
+				InterlockedCompareExchangePointer (( volatile PVOID * )&_pTail->pNode, PreNode.pNode->pNext, PreNode.pNode);
 				continue;
-
 			}
 
 			//Tail의 Next가 NULL일 경우 현재 노드 연결
+			pNode->pNext = NULL;
 			if ( InterlockedCompareExchangePointer (( volatile PVOID * )&_pTail->pNode->pNext, pNode, NULL) == NULL )
 			{
-				InterlockedCompareExchange128 (( volatile LONG64 * )_pTail, Uniqueue, ( LONG64 )pNode, ( LONG64 * )&PreNode);
+
+
+				InterlockedCompareExchange128 (( volatile LONG64 * )_pTail, Uniqueue, ( LONG64 )_pTail->pNode->pNext, ( LONG64 * )&PreNode);
+	//			InterlockedCompareExchangePointer (( volatile PVOID * )&_pTail->pNode, _pTail->pNode->pNext, PreNode.pNode);
 				InterlockedIncrement64 (&_NodeCnt);
 				return true;
 			}
@@ -156,7 +139,6 @@ public :
 	bool Dequeue (DATA *pOut)
 	{
 		_TOP_NODE PreNode;
-		NODE *pNode;
 
 		INT64 Uniqueue = InterlockedIncrement64 (&_UniqueueNum);
 
@@ -164,19 +146,17 @@ public :
 		{
 			PreNode.pNode = _pHead->pNode;
 			PreNode.UNIQUEUE = _pHead->UNIQUEUE;
-			
-			pNode = _pHead->pNode->pNext;
 
 			//헤드의 Next노드를 뽑는 것이므로 pNode가 NULL이라면 Dequeue 불가능.
-			if ( pNode == NULL )
+			if ( PreNode.pNode->pNext == NULL )
 			{
 				pOut = NULL;
 				return false;
 			}
 
-			if ( InterlockedCompareExchange128 (( volatile LONG64 * )_pHead, Uniqueue, ( LONG64 )pNode, ( LONG64 * )&PreNode) )
+			if ( InterlockedCompareExchange128 (( volatile LONG64 * )_pHead, Uniqueue, ( LONG64 )PreNode.pNode->pNext, ( LONG64 * )&PreNode) )
 			{
-				*pOut = pNode->Data;
+				*pOut = PreNode.pNode->pNext->Data;
 				_pMemPool->Free (PreNode.pNode);
 				InterlockedDecrement64 (&_NodeCnt);
 				return true;
